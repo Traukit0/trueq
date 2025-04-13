1a002960f8080fc3769117d1e015e0c57d5d1c46
Traukit0 <mcanonesbet@gmail.com>
2025-04-12 22:43:04 -0400

    Estructura básica del proyecto

---
+++ backend/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from .config import DB_URI
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    CORS(app)
+
+    # Registrar blueprints aquí
+    from .routes.item_routes import item_bp
+    app.register_blueprint(item_bp, url_prefix='/api/items')
+
+    return app
