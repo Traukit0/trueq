caced5f8d5dab3b163df7c6a779aa5bcbac415a8
Traukit0 <mcanonesbet@gmail.com>
2025-05-20 21:19:29 -0400

    Modificado comportamiento de barra lateral, oculta por defecto y se muestra al pasar al mouse cerca de la parte izquierda de la pantalla. Changes to be committed: 	modified:   frontend/src/App.css 	modified:   frontend/src/components/Sidebar.css 	modified:   frontend/src/components/Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,18 +3,21 @@ import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <ul className="sidebar-nav">
-        <li className="sidebar-item">
-          <a href="#" className="sidebar-link">Nuevo TrueQ</a>
-        </li>
-        <li className="sidebar-item">
-          <a href="#" className="sidebar-link">Mis TrueQs</a>
-        </li>
-        <li className="sidebar-item">
-          <a href="#" className="sidebar-link">Dashboard</a>
-        </li>
-      </ul>
+    <div className="sidebar-container">
+      <div className="hover-area"></div>
+      <div className="sidebar">
+        <ul className="sidebar-nav">
+          <li className="sidebar-item">
+            <a href="#" className="sidebar-link">Nuevo TrueQ</a>
+          </li>
+          <li className="sidebar-item">
+            <a href="#" className="sidebar-link">Mis TrueQs</a>
+          </li>
+          <li className="sidebar-item">
+            <a href="#" className="sidebar-link">Dashboard</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
